7aa20c64cecc1dad0b720c2a162500542155c2ad
XeetijDahal <86757471+XeetijDahal@users.noreply.github.com>
2024-09-08 08:47:32 +0545

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,57 +1,108 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Allcountries from "./components/allcountries/Allcountries";
-import Navbar from "./components/navbar/navbar";
-import CountryDetails from "./components/CountryDetails";
+import React, { useEffect, useState } from 'react';
 
-const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [selectedOption, setSelectedOption] = useState("");
-  const [search, setsearch] = useState("");
+const TodoApp = () => {
 
-  useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((response) => response.json())
-      .then((data) => setCountries(data));
-  }, []);
-
-  const handleDropdownChange = (option) => {
-    setSelectedOption(option);
-  };
-  const searchChange = (option) => {
-    setsearch(option);
-  };
-  countries.sort(function (a, b) {
-    if (a.name.common < b.name.common) {
-      return -1;
-    }
-    if (a.name.common > b.name.common) {
-      return 1;
-    }
-    return 0;
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState({
+    name: '',
+    age: '',
+    rollnumber: '',
   });
+  useEffect( () => {
+    localStorage.setItem('kshitij',JSON.stringify(todos))},[todos]
+  )
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewTodo((prevTodo) => ({
+      ...prevTodo,
+      [name]: value,
+    }));
+  };
+
+  const addTodo = () => {
+    if (newTodo.name.trim() === '' || newTodo.age.trim() === '' || newTodo.rollnumber.trim() === '') {
+      return;
+    }
+
+    const newTodos = [
+      ...todos,
+      {
+        id: todos.length + 1,
+        name: newTodo.name,
+        age: newTodo.age,
+        rollnumber: newTodo.rollnumber,
+      },
+    ];
+
+    setTodos(newTodos);
+    setNewTodo({
+      name: '',
+      age: '',
+      rollnumber: '',
+    });
+  };
+
+  const editTodos = (id) => {
+    let changeContent = prompt("What thing do you want to change? ");
+    let value;
+    if (changeContent === "name") {
+      value = prompt("Enter the name: ");
+    } else if (changeContent === "age") {
+      value = prompt("Enter the Age: ");
+    } else if (changeContent === "rollnumber") {
+      value = prompt("Enter the rollnumber: ");
+    }
+    editTodo(id, changeContent, value);
+  };
+
+  const editTodo = (id, fieldName, newValue) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, [fieldName]: newValue } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+  
+
   return (
-    <>
-      <Navbar
-        onSearchChange={searchChange}
-        onDropdownChange={handleDropdownChange}
-      />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Allcountries
-              optn={selectedOption}
-              searching={search}
-              countries={countries}
-              cca2={countries.cca2}
-            />
-          }
-        />
-        <Route path="/:id" element={<CountryDetails countries={countries} />} />
-      </Routes>
-    </>
+    <div>
+      <div>
+        <label>
+          Name:
+          <input type="text" name="name" value={newTodo.name} onChange={handleInputChange} />
+        </label>
+        <label>
+          Age:
+          <input type="text" name="age" value={newTodo.age} onChange={handleInputChange} />
+        </label>
+        <label>
+          Roll Number:
+          <input type="text" name="rollnumber" value={newTodo.rollnumber} onChange={handleInputChange} />
+        </label>
+        <button onClick={addTodo}>Add Student</button>
+      </div>
+      <h1>Student Information</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <strong>Name:</strong> {todo.name}<br/>
+             <strong>Age:</strong> {todo.age}<br/>
+            <strong>Roll Number:</strong> {todo.rollnumber}<br/>
+            <button onClick={() => editTodos(todo.id)}>
+              Edit
+            </button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      
+    </div>
   );
 };
 
-export default App;
+
+export default TodoApp;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,34 +1,12 @@
-import './navbar.css'
-import React, { useState } from "react";
-function Navbar(props){
-    const [selectedOption, setSelectedOption] = useState("");
-
-    const handleChange = (event) => {
-      const option = event.target.value;
-      setSelectedOption(option);
-      props.onDropdownChange(option);
-    };
-    const SearchChange = (event) => {
-      const ans = event.target.value;
-      setSelectedOption(ans);
-      props.onSearchChange(ans);
-    };console.log(selectedOption);
-    return (
-      <div className="nav-items">
-        <h3 className='text-nav'>What do you want to know???</h3>
-        <div className='nav-right'>
-          <input className="search-text" placeholder='Enter the name of a country??? ' onChange={SearchChange} type="text" />
-          <select className="dropdown-btn"onChange={handleChange}>
-            <option value="">All</option>
-            <option value="Africa">Africa</option>
-            <option value="Americas">Americas</option>
-            <option value="Antarctic">Antarctic</option>
-            <option value="Asia">Asia</option>
-            <option value="Europe">Europe</option>
-            <option value="Oceania">Oceania</option>
-          </select>
+import './navbar.css';
+export const Navbar=()=>{
+    return(
+        <div className="Nav-bar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact me</li>
+            </ul>
         </div>
-      </div>
-    );
+    )
 }
-export default Navbar;
